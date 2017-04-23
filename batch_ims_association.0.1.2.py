f2bba0b44184729edd0a8654af0ae3c1370b906e
Lucas Franz Monteiro <lmonteiro@garden.com>
2017-04-23 12:52:46 -0300

    new method 'make_http_request()'

--- batch_ims_association.0.1.2.py
+++ batch_ims_association.0.1.2.py
@@ -14,27 +14,73 @@ user = None
 password = None
 
 def set_configurations():
-    configurations_file = '/home/lmonteiro/configurations.json'
+    configurations_file = '/home/lmonteiro/Documents/scripts/SM/configurations.json'
     with open(configurations_file) as f:
         configurations = json.load(f)
     global rest_api_prefix, user, password    
     rest_api_prefix = configurations['rest_api_prefix']
     user = configurations['authentication']['user']
-    password = base64
-               .b64decode(configurations['authentication']['password'])
-               .decode('ascii')
+    password = base64.b64decode(configurations['authentication']['password']).decode('ascii')
     return configurations
-    
+
+def make_http_request(url, content=None, http_method='GET', request_timeout=1):
+    max_attempts = 3
+    attempts = 0
+    errors = []
+    while attempts < max_attempts:
+        try:
+            error = {}
+            error['is_unknown'] = False
+            
+            if http_method.upper() == 'GET':
+                response = requests.get(url, auth=(user, password)) #TODO: Include timeout
+            elif http_method.upper() == 'POST':
+                if content is None:
+                    error['message'] = 'POST request withouth content.'
+                    #return False
+                response = requests.post(url, auth=(user, password), json=content) #TODO: Include timeout
+            else:
+                print 'Unknown HTTP method.' #TODO: Improve this
+            #
+            if response.status_code == 200:
+                return { 'successful_http_request': True,
+                         'response': response }
+            elif response.status_code == 401:
+                error['message'] = 'Unauthorized request, check your credentials.'
+            
+            attempts += 1
+            errors.append(error)
+            if attempts < max_attempst:
+                print 'Trying again ..'
+            continue
+        except ReadTimeout:
+            error['message'] = 'Request timed out.'
+        except Exception:
+            error['is_unknown'] = True
+            error['message'] = 'Unknown error while '
+            
+    print 'Request aborted. Three attempts failed.'
+    return { 'successful_http_request': False,
+             'response': errors }
+
 #TODO: Check if incidents exist.
 def get_incident_details(incident_id):
-    url = '{0}/incidents?IncidentID={1}&view=expand'
-          .format(rest_api_prefix, incident_id)
-    response = requests.get(url, auth=(user, password)) #TODO: Include timeout.
-    return json.loads(response.text)['content'][0]['Incident']
+    url = '{0}/incidents?IncidentID={1}&view=expand'.format(rest_api_prefix, incident_id)
+    result = make_http_request(url)
+    if result['successful_request'] is True:
+        response = result['response']
+        return json.loads(response.text)['content'][0]['Incident']
+    """print '> Error messages:'
+    for error_message in result['error_messages']:
+        if error_message is None:
+            error_message = 'Unknown error while getting {0} details.'.format(incident_id)
+        print error_message
+    return False"""
+    #TODO: Reorganize this.
     
 def is_association_allowed(source_incident, dependent_incident):
     forbidden_services = ['swap', 'disk_usage'] # Dependent incidents with these services can not be associated.
-    dependent_incident_service = dependent_incident['BriefDescription'][1] #TODO: Use split() ?
+    dependent_incident_service = dependent_incident['BriefDescription'].split()[1] #TODO: Use split() ?
     is_allowed = True
     why_not = [] # Reasons why the association is not allowed.
     
@@ -61,40 +107,24 @@ def is_association_allowed(source_incident, dependent_incident):
     return { dependent_incident['IncidentID']: is_allowed if is_allowed else why_not }
     
 def update_incident_status(incident_id, new_status):
-    url = '{0}/incidents/{1}/action/update'
-          .format(rest_api_prefix, incident_id)
-          
+    url = '{0}/incidents/{1}/action/update'.format(rest_api_prefix, incident_id)
     incident_update = { 'Incident': {
                             'AssigneeName': user,
                             'IMTicketStatus': new_status,
                             'update.action': incident_id
                         }
                       }
-    maximum_attempts = 3
-    attempts = 0
-    error_messages = []
-    while attempts < maximum_attempts:
-        try:
-            response = requests.post(url,
-                                 auth=(user, password),
-                                 json=incident_update)
-            print 'Updating {0} status to {1}.'.format(incident_id, new_status)
-            #print 'HTTP status code: {0}.'.format(response.status_code)
-            if response.status_code == 200: #TODO: Check if status is unauthorized (401)
-                return True
-            attempts += 1
-        except ReadTimeout:
-            error_messages.append('Request timed out.')
-        except Exception:
-            error_messages.append('Unknown error while updating {0} status.'
-                                  .format(incident_id))
-        print error_messages[len(error_messages) - 1]
+    result = make_http_request(url, content=incident_update, http_method='POST')
+    print 'Updating {0} status to {1}.'.format(incident_id, new_status)
+    #TODO: Use something like 'result.items()' to define a proper unknown error message.
+    """for error_message in result['error_messages']:
+        if error_message['is_unknown']
+            error_message['is_unknown'] += ' updating {0} status.'.format(incident_id)"""
         
-    return { incident_id: error_messages }
+    return result
 
 def associate_incidents(source_incident_id, dependent_incident_id):
     url = '{0}/screlations'.format(rest_api_prefix)
-    
     new_association = { 'screlation': {
                             'Depend': dependent_incident_id,
                         	'Depend_Filename': 'problem',
@@ -102,28 +132,10 @@ def associate_incidents(source_incident_id, dependent_incident_id):
                             'Source_Filename': 'problem'
                         }
                       }
-    maximum_attempts = 3
-    attempts = 0
-    error_messages = []
-    while attempts < maximum_attempts:
-        try:
-            response = requests.post(url,
-                                     auth=(user, password),
-                                     json=new_association)
-            print 'Associating incidents {0} and {1}.'
-                  .format(dependent_incident_id, source_incident_id)
-            #print 'HTTP status code: {0}.'.format(response.status_code)
-            #print 'Message: {0}\n'.format(response.text)
-            if response.status_code == 200:
-                return { dependent_incident_id: True }
-            attempts += 1
-        except ReadTimeout:
-            error_messages.append('Request timed out.')
-        except Exception:
-            error_messages.append('Unknown error while associating {0} with {1}.'
-                                  .format(dependent_incident_id, source_incident_id))
-        print error_messages[len(error_messages) - 1]
-    return { dependent_incident_id: error_messages }
+    result = make_http_request(url, content=new_association, http_method='POST')
+    print 'Associating incidents {0} and {1}.'.format(dependent_incident_id, source_incident_id)
+    return result
+    #'Unknown error while associating {0} with {1}.'.format(dependent_incident_id, source_incident_id))
 
 def associate_incident_set_to_source(source_incident_id, dependent_incidents_ids):
     successful_associations = []
@@ -138,7 +150,7 @@ def associate_incident_set_to_source(source_incident_id, dependent_incidents_ids
             continue
             
         response = associate_incidents(source_incident_id,
-                                       dependent_incident_id):
+                                       dependent_incident_id)
         if response[dependent_incident_id] is True:
             successful_associations.append(dependent_incident_id)
             update_incident_status(dependent_incident_id, pending_status)
@@ -183,7 +195,7 @@ if __name__ == "__main__":
             pass #TODO: implement it.
     
     set_configurations()
-    dependent_incidents_ids = list(set((dependent_incidents_ids)) # remove repeated incidents
+    dependent_incidents_ids = list(set(dependent_incidents_ids)) # remove repeated incidents
     #print 'Source incident: {0}'.format(source_incident)
     #print 'Dependent incidents: {0}'.format(', '.join(dependent_incidents))
     #sys.exit(0)
